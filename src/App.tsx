62b14abc6907fa44ec64958378886a704ad1f085
dan.jorgensen <dan.jorgensen@keylane.com>
2024-10-25 14:41:14 +0200

    Fix error and add new feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,50 @@
-import React from 'react';
-import PhotoAlbumComponent from './PhotoAlbum';
-import Slider from './Slider';
+import React from "react";
+import PhotoAlbumComponent from "./PhotoAlbum";
+import Slider from "./Slider";
 
 const App: React.FC = () => {
   const [sliderValue, setSliderValue] = React.useState<number>(0);
-  const week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+  const [audio, setAudio] = React.useState<HTMLAudioElement | null>(null);
+
+  const week = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+
+  const playSound = () => {
+    if (audio) {
+      audio.pause();
+    }
+
+    setAudio(new Audio(`test.mp3#t=00:00:0${sliderValue*2}`));
+
+    if (audio) {
+      audio.play();
+
+      setTimeout(function () {
+        audio.pause();
+      }, 2000);
+    }
+  };
+
   return (
     <div>
+      {sliderValue}
       <h1>Week mood {week[sliderValue]}</h1>
-      <Slider min={0} max={6} initialValue={0} onChange={() => setSliderValue(sliderValue/1-1)} />
+      <Slider
+        min={0}
+        max={6}
+        initialValue={0}
+        onChange={(value) => {
+          setSliderValue(value);
+          playSound();
+        }}
+      />
       <PhotoAlbumComponent index={sliderValue} />
     </div>
   );
